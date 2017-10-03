d137f87d22b48a413d447fddb5d519914dbbd43e
KC345 <32089619+KC345@users.noreply.github.com>
2017-10-03 09:10:56 +0200

    Frame erstellt und hintergrund eingefügt

--- src/Menue.java
+++ src/Menue.java
@@ -1,12 +1,26 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Menue {
 
     JFrame frame = new JFrame("Maze Runner Multiplayer");
+    JLabel picLabel;
+
 
     public Menue(){
 
-
+        BufferedImage myPicture = null;
+        try {
+            myPicture = ImageIO.read(new File("src/Maze_Background.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        picLabel = new JLabel(new ImageIcon(myPicture));
+        frame.add(picLabel);
+        frame.setSize(705,705);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
