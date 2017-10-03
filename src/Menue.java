72bfef52cab851b11bafa7af8e6cac4d880e02f9
KC345 <32089619+KC345@users.noreply.github.com>
2017-10-03 09:29:43 +0200

    Button eingefügt, sind aber noch nicht an der Richtigen stelle

--- src/Menue.java
+++ src/Menue.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,9 +9,14 @@ public class Menue {
 
     JFrame frame = new JFrame("Maze Runner Multiplayer");
     JLabel picLabel;
+    JButton solo = new JButton("Einzelspieler");
+    JButton multi = new JButton("Multiplayer");
+    JButton cont = new JButton("Steuerung anzeigen");
 
 
     public Menue(){
+
+        frame.setLayout(new GridLayout(1,1));
 
         BufferedImage myPicture = null;
         try {
@@ -19,7 +25,16 @@ public class Menue {
             e.printStackTrace();
         }
         picLabel = new JLabel(new ImageIcon(myPicture));
+        picLabel.setLayout(new FlowLayout());
+
+        solo.setBounds(200,150,50,50);
+
+        picLabel.add(solo);
+        picLabel.add(multi);
+        picLabel.add(cont);
+
         frame.add(picLabel);
+
         frame.setSize(705,705);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
