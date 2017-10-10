45932e77b3a7a22552319462f77d7800d5cb4456
blackdevil8975 <cflololol699@gmail.com>
2017-10-10 09:31:52 +0200

    Java / Server ersterstellung

--- src/Menue.java
+++ src/Menue.java
@@ -4,19 +4,20 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class Menue {
 
     JFrame frame = new JFrame("Maze Runner Multiplayer");
     JLabel picLabel;
+    JPanel buttonPannel = new JPanel();
     JButton solo = new JButton("Einzelspieler");
     JButton multi = new JButton("Multiplayer");
     JButton cont = new JButton("Steuerung anzeigen");
 
-
     public Menue(){
 
-        frame.setLayout(new GridLayout(1,1));
+
 
         BufferedImage myPicture = null;
         try {
@@ -24,21 +25,40 @@ public class Menue {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        picLabel = new JLabel(new ImageIcon(myPicture));
-        picLabel.setLayout(new FlowLayout());
+       // picLabel = new JLabel(new ImageIcon(myPicture));
+        // picLabel.setLayout(new FlowLayout());
+       // picLabel.setBounds(700, 700,400,100);
 
-        solo.setBounds(200,150,50,50);
+       // solo.setBounds(200,0,50,50);
+        //multi.setBounds(200,50,50,50);
+        //cont.setBounds(200,100,50,50);
+        solo.setVisible(true);
+        multi.setVisible(true);
+        cont.setVisible(true);
+        //buttonPannel.add(picLabel);
 
-        picLabel.add(solo);
-        picLabel.add(multi);
-        picLabel.add(cont);
+        buttonPannel.add(solo);
+        buttonPannel.add(multi);
+        buttonPannel.add(cont);
+        buttonPannel.setLayout(new GridLayout(3,1));
+        buttonPannel.setBounds(150, 250,400,150);
+        buttonPannel.setVisible(true);
+        buttonPannel.setBackground(new Color(0f,0f,0f,0));
 
-        frame.add(picLabel);
 
+
+        //frame.add(picLabel);
+        frame.setContentPane(new JLabel(new ImageIcon(myPicture)));
+        frame.add(buttonPannel);
+        frame.setLayout(null);
         frame.setSize(705,705);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
+
     }
+
 
     public static void main(String [] args){
         new Menue();
