11c737af21af39a7abcad4ba433801cdf815ccd6
blackdevil8975 <cflololol699@gmail.com>
2017-10-17 09:01:39 +0200

    Multiplayer Menü ausprogrammiert, Zurück Buttons hinzugefügt

--- src/Multiplayer.java
+++ src/Multiplayer.java
@@ -1,18 +1,70 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class Multiplayer {
+public class Multiplayer implements ActionListener{
 
-    JFrame multiframe;
+    JFrame multiframe = new JFrame("Maze Runner Multiplayer");
+    JPanel buttonPannel = new JPanel();
+    JButton client = new JButton("Client");
+    JButton server = new JButton("Server");
+    JButton back;
+
+
+
 
     public Multiplayer(){
 
-        multiframe = new JFrame("Maze Runner Multiplayer");
+        BufferedImage myPicture = null;
+        try {
+            myPicture = ImageIO.read(new File("src/Maze_Background.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+
+        client.setVisible(true);
+        server.setVisible(true);
+
+        buttonPannel.add(client);
+        buttonPannel.add(server);
+        back = new JButton("Zurück");
+        buttonPannel.add(back);
+        back.addActionListener(this);
+        buttonPannel.setLayout(new GridLayout(3,1));
+        buttonPannel.setBounds(150, 250,400,150);
+        buttonPannel.setVisible(true);
+        buttonPannel.setBackground(new Color(0f,0f,0f,0));
+
+
+
+
+        multiframe.setContentPane(new JLabel(new ImageIcon(myPicture)));
+        multiframe.add(buttonPannel);
+        multiframe.setLayout(null);
+        multiframe.setSize(705,705);
+        multiframe.setResizable(false);
         multiframe.pack();
         multiframe.setLocationRelativeTo(null);
         multiframe.setVisible(true);
-        multiframe.setSize(705,705);
-        multiframe.setResizable(false);
         multiframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+
     }
+
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == back){
+                new Menue();
+                multiframe.dispose();
+            }
+        }
+    public static void main(String [] args){
+        new Multiplayer();
+    }
+
 }
